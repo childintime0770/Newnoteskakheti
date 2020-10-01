36b1a4bd56ed251873b48741eacd3848b12b3863
childintime0770 <71454077+childintime0770@users.noreply.github.com>
2020-10-01 15:48:13 +0400

    Add files via upload

--- mynote.js
+++ mynote.js
@@ -5,7 +5,9 @@ let noteSaverButton = document.getElementById('notesaver');
 let notelist = document.getElementById('list');
 let note1;
 let savedNote;
-let firstWord; 
+let firstWord;  
+let dateParagraph;
+let presentTime = new Date(); 
 
 
 addButton.onclick = function() {
@@ -13,16 +15,21 @@ addButton.onclick = function() {
     noteSaverButton.style.visibility = 'visible';
     note1 = document.createElement('li');
     notelist.appendChild(note1);
-    note1.setAttribute('class', 'saved')
+    note1.setAttribute('class', 'saved');
     
 }
 
 
 noteSaverButton.onclick = function() {
     savedNote = maneTextarea.value;
-    firstWord = savedNote.split(' ', 1);
+    firstWord = savedNote.split('\n', 1)[0];
     note1.textContent = firstWord;
+    dateParagraph = document.createElement('p');
+    note1.appendChild(dateParagraph);
+    dateParagraph.setAttribute('class', 'time');
     
+
+
 }
 
 
